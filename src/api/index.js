a76ffd5478efc5a4eeaabca9863955c481526128
Jake <jakerfilut@gmail.com>
2022-04-06 23:15:13 -0600

    imported API

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,28 @@
 import axios from "axios";
 
-const getPlacesData = async () => {
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+const options = {
+  method: "GET",
+  params: {
+    bl_latitude: "11.847676",
+    tr_latitude: "12.838442",
+    bl_longitude: "109.095887",
+    tr_longitude: "109.149359",
+  },
+  headers: {
+    "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+    "X-RapidAPI-Key": "6c0baa9409mshb1ecd42e036b587p14dfacjsne22ee1d72cbc",
+  },
+};
+
+export const getPlacesData = async () => {
   try {
+    const {
+      data: { data },
+    } = await axios.get(URL, options);
+
+    return data;
   } catch (errer) {}
 };
